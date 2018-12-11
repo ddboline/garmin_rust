b7597c2520c962d7025a511d14ac15223058ca9b
Daniel Boline <ddboline@gmail.com>
2018-12-11 11:47:35 -0500

    updates

--- build_deb.sh
+++ build_deb.sh
@@ -3,10 +3,6 @@
 VERSION="$1"
 RELEASE="$2"
 
-source ~/.cargo/env
-
-rustup default nightly
-
 cargo build --release
 printf "\ninstall:\n\tcp target/release/garmin_rust_proc target/release/garmin_rust_report target/release/garmin_rust_http /usr/bin/\n" > Makefile
 printf "Process and display info about gps activity files\n" > description-pak

9ba05707ddc342dcd73f6438d7ad7d37efd77ccd
Daniel Boline <ddboline@gmail.com>
2018-12-18 05:52:17 -0500

    convert strava_upload from cli app to simple flask api

--- python/strava_upload.py
+++ python/strava_upload.py
@@ -1,258 +1,146 @@
 #!/usr/bin/env python3
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-import webbrowser
 import os.path
 import gzip
-import argparse
 import requests
-import socket
-from six.moves.urllib import parse as urlparse
-
-from six.moves.BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 from stravalib import Client, exc
-from sys import stderr, stdin
 from tempfile import NamedTemporaryFile
-
-try:
-    from lxml import etree
-except ImportError:
-    import xml.etree.ElementTree as etree
 
 try:
     from ConfigParser import ConfigParser
 except ImportError:
     from configparser import ConfigParser
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
+from flask import Flask, request
+
+app = Flask(__name__)
+
+ACTIVITY_TYPES = ('ride', 'run', 'swim', 'workout', 'hike', 'walk', 'nordicski', 'alpineski',
+                  'backcountryski', 'iceskate', 'inlineskate', 'kitesurf', 'rollerski', 'windsurf',
+                  'snowboard', 'snowshoe')
 
 
-def strava_upload():
-    """
-        upload to strava, borrowed from https://github.com/dlenski/stravacli
-    """
+def get_config():
+    cp_ = ConfigParser()
+    cp_.read(os.path.expanduser('~/.stravacli'))
+    cat = None
+    if cp_.has_section('API'):
+        cid = cp_.get('API', 'CLIENT_ID')
+        cs = cp_.get('API', 'CLIENT_SECRET')
+        if 'access_token' in cp_.options('API'):
+            cat = cp_.get('API', 'ACCESS_TOKEN')
+    return cp_, cid, cs, cat
+
+
+@app.route('/callback', methods=['GET'])
+def strava_auth_callback():
+    cp_, cid, cs, cat = get_config()
+
+    code = request.args.get('code')
+
+    if code is None:
+        return 'No code received', 200
+
+    client = Client()
+
+    cat = client.exchange_code_for_token(client_id=cid, client_secret=cs, code=code)['access_token']
+
+    print(cid, cs, cat)
+
+    if not cp_.has_section('API'):
+        cp_.add_section('API')
+    cp_.set('API', 'CLIENT_ID', cid)
+    cp_.set('API', 'CLIENT_SECRET', cs)
+    cp_.set('API', 'ACCESS_TOKEN', cat)
+    cp_.write(open(os.path.expanduser('~/.stravacli'), "w"))
+
+    return '<title>Strava auth code received!</title>This window can be closed.', 200
+
+
+@app.route('/', methods=['POST'])
+def strava_endpoint():
     allowed_exts = {
         '.tcx': lambda v: '<TrainingCenterDatabase' in v[:200],
         '.gpx': lambda v: '<gpx' in v[:200],
         '.fit': lambda v: v[8:12] == '.FIT'
     }
 
-    par = argparse.ArgumentParser(description='Uploads activities to Strava.')
-    par.add_argument(
-        'activities',
-        nargs='*',
-        type=argparse.FileType("rb"),
-        default=(stdin, ),
-        help="Activity files to upload (plain or gzipped {})".format(', '.join(allowed_exts)))
-    par.add_argument(
-        '-P', '--no-popup', action='store_true', help="Don't browse to activities after upload.")
-    par.add_argument(
-        '-E',
-        '--env',
-        help='Look for ACCESS_TOKEN in environment variable '
-        'rather than ~/.stravacli')
-    grp = par.add_argument_group('Activity file details')
-    grp.add_argument('-p', '--private', action='store_true', help='Make activities private')
-    grp.add_argument(
-        '-t',
-        '--type',
-        choices=allowed_exts,
-        default=None,
-        help='Force files to be interpreted as being of given '
-        'type (default is to autodetect based on name, or '
-        'contents for stdin)')
-    grp.add_argument(
-        '-x',
-        '--xml-desc',
-        action='store_true',
-        help='Parse name/description fields from GPX and TCX '
-        'files.')
-    grp.add_argument('-T', '--title', help='Activity title')
-    grp.add_argument('-D', '--desc', dest='description', help='Activity description')
-    grp.add_argument(
-        '-A',
-        '--activity-type',
-        default=None,
-        help='Type of activity. If not specified, the default '
-        'value is taken from user profile. '
-        'Supported values: \n\t ride, run, swim, workout, '
-        'hike, walk, nordicski, alpineski, backcountryski, '
-        'iceskate, inlineskate, kitesurf, rollerski, '
-        'windsurf, workout, snowboard, snowshoe')
-    args = par.parse_args()
+    print(request.json.get('filename'))
+    print(request.json.get('title'))
+    print(request.json.get('activity_type'))
 
-    if args.xml_desc:
-        if args.title:
-            print('argument -T/--title not allowed with argument ' '-x/--xml-desc', file=stderr)
-        if args.description:
-            print('argument -D/--desc not allowed with argument ' '-x/--xml-desc', file=stderr)
+    filename = request.json['filename']
+    title = request.json['title']
+    activity_type = request.json['activity_type']
 
-    # Authorize Strava
-    cp_ = ConfigParser()
-    cp_.read(os.path.expanduser('~/.stravacli'))
-    cat = None
-    if cp_.has_section('API'):
-        if 'access_token' in cp_.options('API'):
-            cat = cp_.get('API', 'ACCESS_TOKEN')
-            cs = cp_.get('API', 'CLIENT_SECRET')
-            cat = cp_.get('API', 'ACCESS_TOKEN')
+    description = request.json.get('description')
+    is_private = request.json.get('private', False)
 
-    while True:
-        client = Client(cat)
-        try:
-            athlete = client.get_athlete()
-        except requests.exceptions.ConnectionError:
-            print("Could not connect to Strava API", file=stderr)
-        except Exception as e:
-            print("NOT AUTHORIZED %s" % e, file=stderr)
-            print("Need Strava API access token. Launching web browser to "
-                  "obtain one.",
-                  file=stderr)
-            client = Client()
-            webserver = QueryGrabber(
-                response='<title>Strava auth code received!</title>This window can be closed.',
-                address=('localhost', 52168))
-            _scope = 'view_private,write'
-            authorize_url = client.authorization_url(
-                client_id=cid, redirect_uri=webserver.root_uri(), scope=_scope)
-            webbrowser.open_new_tab(authorize_url)
-            webserver.handle_request()
-            client.access_token = cat = client.exchange_code_for_token(
-                client_id=cid, client_secret=cs, code=webserver.received['code'])
-            cp_.add_section('API')
-            cp_.set('API', 'CLIENT_ID', cid)
-            cp_.set('API', 'CLIENT_SECRET', cs)
-            cp_.set('API', 'ACCESS_TOKEN', cat)
-            cp_.write(open(os.path.expanduser('~/.stravacli'), "w"))
+    assert activity_type in ACTIVITY_TYPES, 'invalid activity'
+
+    cp_, cid, cs, cat = get_config()
+
+    client = Client(cat)
+
+    try:
+        client.get_athlete()
+    except requests.exceptions.ConnectionError:
+        raise
+    except Exception:
+        client = Client()
+
+        _scope = 'activity:write'
+        authorize_url = client.authorization_url(
+            client_id=cid, redirect_uri='https://www.ddboline.net/strava/callback', scope=_scope)
+
+        return f'<a href="{authorize_url}" target="_blank">Link</a>', 200
+
+    act = open(filename, 'rb')
+
+    base, ext = os.path.splitext(act.name)
+    # autodetect based on extensions
+    if ext.lower() == '.gz':
+        base, ext = os.path.splitext(base)
+        # un-gzip it in order to parse it
+        cf_ = act
+    else:
+        cf_ = NamedTemporaryFile(suffix='.gz')
+        gzip.GzipFile(fileobj=cf_, mode='w+b').writelines(act)
+    if ext.lower() not in allowed_exts:
+        return "Don't know how to handle extension " \
+               "{} (allowed are {}).".format(ext, ', '.join(allowed_exts)), 400
+
+    # upload activity
+    try:
+        cf_.seek(0, 0)
+        upstat = client.upload_activity(
+            cf_,
+            ext[1:] + '.gz',
+            title,
+            description,
+            private=is_private,
+            activity_type=activity_type)
+        activity = upstat.wait()
+        activity_id = activity.id
+        duplicate = False
+    except exc.ActivityUploadFailed as e:
+        words = e.args[0].split()
+        print(words)
+        if words[-4:-1] == ['duplicate', 'of', 'activity']:
+            activity_id = int(words[-1])
+            duplicate = True
         else:
-            if not cp_.has_section('API'):
-                cp_.add_section('API')
-            if 'ACCESS_TOKEN' not in cp_.options('API') or cp_.get('API', 'ACCESS_TOKEN',
-                                                                   None) != cat:
-                cp_.set('API', 'ACCESS_TOKEN', cat)
-                cp_.write(open(os.path.expanduser('~/.stravacli'), "w"))
-            break
+            raise
 
-    print("Authorized to access account of {} {} (id {:d}).".format(athlete.firstname,
-                                                                    athlete.lastname, athlete.id))
-
-    for act in args.activities:
-        if act is stdin:
-            contents = act.read()
-            act = StringIO(contents)
-            if args.type is None:
-                # autodetect gzip and extension based on content
-                if contents.startswith('\x1f\x8b'):
-                    gz_, cf_, uf_ = '.gz', act, gzip.GzipFile(fileobj=act, mode='rb')
-                    contents = uf_.read()
-                else:
-                    gz_, uf_, cf_ = '', act, NamedTemporaryFile(suffix='.gz')
-                    gzip.GzipFile(fileobj=cf_, mode='w+b').writelines(act)
-                for ext, checker in allowed_exts.items():
-                    if checker(contents):
-                        print("Uploading {} activity from stdin...".format(ext + gz_))
-                        break
-                else:
-                    print("Could not determine file type of stdin", file=stderr)
-            else:
-                base, ext = 'activity', args.type
-        else:
-            base, ext = os.path.splitext(act.name if args.type is None else 'activity.' + args.type)
-            # autodetect based on extensions
-            if ext.lower() == '.gz':
-                base, ext = os.path.splitext(base)
-                # un-gzip it in order to parse it
-                gz_, cf_, uf_ = '.gz', act, None if args.no_parse else \
-                                gzip.GzipFile(fileobj=act, mode='rb')
-            else:
-                gz_, uf_, cf_ = '', act, NamedTemporaryFile(suffix='.gz')
-                gzip.GzipFile(fileobj=cf_, mode='w+b').writelines(act)
-            if ext.lower() not in allowed_exts:
-                print(
-                    "Don't know how to handle extension "
-                    "{} (allowed are {}).".format(ext, ', '.join(allowed_exts)),
-                    file=stderr)
-            print("Uploading {} activity from {}...".format(ext + gz_, act.name))
-
-        # try to parse activity name, description from file if requested
-        if args.xml_desc:
-            uf_.seek(0, 0)
-            if ext.lower() == '.gpx':
-                x = etree.parse(uf_)
-                nametag, desctag = x.find("{*}name"), x.find("{*}desc")
-                title = nametag and nametag.text
-                desc = desctag and desctag.text
-            elif ext.lower() == '.tcx':
-                x = etree.parse(uf_)
-                notestag = x.find("{*}Activities/{*}Activity/{*}Notes")
-                if notestag is not None:
-                    title, desc = (notestag.text.split('\n', 1) + [None])[:2]
-        else:
-            title = args.title
-            desc = args.description
-
-        # upload activity
-        try:
-            cf_.seek(0, 0)
-            upstat = client.upload_activity(
-                cf_,
-                ext[1:] + '.gz',
-                title,
-                desc,
-                private=args.private,
-                activity_type=args.activity_type)
-            activity = upstat.wait()
-            duplicate = False
-        except exc.ActivityUploadFailed as e:
-            words = e.args[0].split()
-            if words[-4:-1] == ['duplicate', 'of', 'activity']:
-                activity = client.get_activity(words[-1])
-                duplicate = True
-            else:
-                raise
-
-        # show results
-        uri = "http://strava.com/activities/{:d}".format(activity.id)
-        print("  {}{}".format(uri, " (duplicate)" if duplicate else ''), file=stderr)
-        if not args.no_popup:
-            webbrowser.open_new_tab(uri)
-
-
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.server.received = urlparse.parse_qs(self.path.split('?', 1)[1])
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(self.server.response)
-
-
-class QueryGrabber(HTTPServer):
-
-    def __init__(self, response='', address=None):
-        self.received = None
-        self.response = response
-        if address != None:
-            HTTPServer.__init__(self, address, handler)
-        else:
-            for port in range(1024, 65536):
-                try:
-                    HTTPServer.__init__(self, ('localhost', port), handler)
-                except socket.error as e:
-                    if e.errno != 98:  # Address already in use
-                        raise
-                else:
-                    break
-            else:
-                raise Exception('No open ports')
-
-    def root_uri(self):
-        return 'http://{}:{:d}'.format(*self.server_address)
+    uri = "http://strava.com/activities/{:d}".format(activity_id)
+    # show results
+    if duplicate:
+        return uri, 200
+    else:
+        return uri, 200
 
 
 if __name__ == '__main__':
-    strava_upload()
+    app.run(host='0.0.0.0', port=52168)

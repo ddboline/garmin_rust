3c98ce4bd1a2cfd09ebca95d88f333f9bf1d2e54
Daniel Boline <ddboline@gmail.com>
2019-06-12 06:26:49 -0400

    error_response -> render_response

--- garmin_http/src/errors.rs
+++ garmin_http/src/errors.rs
@@ -1,6 +1,6 @@
 use failure::Fail;
 
-use actix_web::{error::ResponseError, http::StatusCode, HttpResponse};
+use actix_web::{error::ResponseError, HttpResponse};
 
 #[derive(Fail, Debug)]
 pub enum ServiceError {
@@ -16,13 +16,13 @@ pub enum ServiceError {
 
 // impl ResponseError trait allows to convert our errors into http responses with appropriate data
 impl ResponseError for ServiceError {
-    fn error_response(&self) -> HttpResponse {
+    fn render_response(&self) -> HttpResponse {
         match *self {
             ServiceError::InternalServerError => {
                 HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
             }
             ServiceError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
-            ServiceError::Unauthorized => HttpResponse::build(StatusCode::OK)
+            ServiceError::Unauthorized => HttpResponse::Ok()
                 .content_type("text/html; charset=utf-8")
                 .body(
                     include_str!("../../templates/login.html")

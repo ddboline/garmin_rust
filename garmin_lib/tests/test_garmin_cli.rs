59e0be9297ce985f905eaa36405db64d4c9ded74
Daniel Boline <dboline@mediamath.com>
2019-06-18 16:09:44 -0400

    experiment with doctest

--- garmin_lib/tests/test_garmin_cli.rs
+++ garmin_lib/tests/test_garmin_cli.rs
@@ -4,17 +4,11 @@ use garmin_lib::common::garmin_correction_lap::GarminCorrectionList;
 use garmin_lib::parsers::garmin_parse::GarminParse;
 
 #[test]
-fn test_garmin_cli_new() {
-    let gcli = GarminCliObj::<GarminParse, GarminCorrectionList>::new();
-    assert_eq!(gcli.opts, None);
-}
-
-#[test]
 fn test_garmin_file_test_filenames() {
     let test_config = "tests/data/test.env";
 
     let gcli = GarminCliObj {
-        config: garmin_config::GarminConfig::get_config(Some(test_config)),
+        config: garmin_config::GarminConfig::get_config(Some(test_config)).unwrap(),
         opts: Some(GarminCliOptions::FileNames(vec![
             "tests/data/test.fit".to_string(),
             "tests/data/test.gmn".to_string(),

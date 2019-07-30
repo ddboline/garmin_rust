3728840893f7f0c0ba7c1f7890f73aedb857377e
Daniel Boline <ddboline@gmail.com>
2019-07-30 15:43:06 +0000

    bugfix

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -13,6 +13,6 @@ mkdir -p backup/
 
 for T in $TABLES;
 do
-    aws s3 cp s3://${BACKUP}/${T}.sql.gz backup/${T}.sql.gz
+    aws s3 cp s3://${BUCKET}/${T}.sql.gz backup/${T}.sql.gz
     gzip -dc backup/${T}.sql.gz | psql $DB -c "COPY $T FROM STDIN"
 done

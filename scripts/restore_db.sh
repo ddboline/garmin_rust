aa9b2fd4c0bb1fda3021242151f4fb6b2975d54c
Daniel Boline <ddboline@gmail.com>
2019-08-17 21:10:13 +0000

    fix seq

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -17,3 +17,6 @@ do
     aws s3 cp s3://${BUCKET}/${T}.sql.gz backup/${T}.sql.gz
     gzip -dc backup/${T}.sql.gz | psql $DB -c "COPY $T FROM STDIN"
 done
+
+psql $DB -c "select setval('garmin_corrections_laps_id_seq', (select max(index) from garmin_corrections_laps), TRUE)"
+psql $DB -c "select setval('scale_measurements_id_seq', (select max(index) from scale_measurements), TRUE)"

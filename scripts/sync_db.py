b8dacd14f2eb6b555027ca1b5577446f58ee24a2
Daniel Boline <ddboline@gmail.com>
2019-09-03 22:52:26 -0400

    ...

--- scripts/sync_db.py
+++ scripts/sync_db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import requests
 import os
-from urllib.parse import urlencode
 
 
 def read_config_env():
@@ -31,21 +30,36 @@ def sync_db():
     from_endpoint = 'https://cloud.ddboline.net'
     to_endpoint = 'https://www.ddboline.net'
 
-    cookies0 = requests.post(
-        f'{from_endpoint}/api/auth', json={
-            'email': garmin_username,
-            'password': garmin_password
-        }).cookies
-    cookies1 = requests.post(
-        f'{to_endpoint}/api/auth', json={
-            'email': garmin_username,
-            'password': garmin_password
-        }).cookies
+    cookies0 = requests.post(f'{from_endpoint}/api/auth',
+                             json={
+                                 'email': garmin_username,
+                                 'password': garmin_password
+                             }).cookies
+    cookies1 = requests.post(f'{to_endpoint}/api/auth',
+                             json={
+                                 'email': garmin_username,
+                                 'password': garmin_password
+                             }).cookies
 
-    last_modified0 = requests.get(f'{from_endpoint}/garmin/scale_measurements', cookies=cookies0).json()
-    last_modified1 = requests.post(f'{to_endpoint}/garmin/scale_measurements', cookies=cookies1, json={'measurements': last_modified0}).json()
-    print(last_modified1)
+    measurements0 = requests.get(f'{from_endpoint}/garmin/scale_measurements',
+                                 cookies=cookies0).json()
+    measurements1 = requests.get(f'{to_endpoint}/garmin/scale_measurements',
+                                 cookies=cookies1).json()
+    measurements0 = {x['datetime']: x for x in measurements0}
+    measurements1 = {x['datetime']: x for x in measurements1}
 
+    measurements = [measurements0[x] for x in (set(measurements0) - set(measurements1))]
+    if len(measurements) > 0:
+        print(measurements)
+        requests.post(f'{to_endpoint}/garmin/scale_measurements',
+                      cookies=cookies1,
+                      json={'measurements': measurements})
+    measurements = [measurements1[x] for x in (set(measurements1) - set(measurements0))]
+    if len(measurements) > 0:
+        print(measurements)
+        requests.post(f'{from_endpoint}/garmin/scale_measurements',
+                      cookies=cookies0,
+                      json={'measurements': measurements})
     return
 
 

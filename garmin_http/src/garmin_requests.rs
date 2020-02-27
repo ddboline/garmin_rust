9a228ee4df7c84b881c1436f0c9d80ef145a5ab2
Daniel Boline <ddboline@gmail.com>
2020-02-27 08:48:34 -0500

    fix typo

--- garmin_http/src/garmin_requests.rs
+++ garmin_http/src/garmin_requests.rs
@@ -460,7 +460,7 @@ impl HandleRequest<ScaleMeasurementUpdateRequest> for PgPool {
             async move {
                 if !measurement_set.contains(&meas.datetime) {
                     meas.insert_into_db(self).await?;
-                    debug!("measurement inserted {:?}" meas);
+                    debug!("measurement inserted {:?}", meas);
                 } else {
                     debug!("measurement exists {:?}", meas);
                 }

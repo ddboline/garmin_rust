a5505fd8546446517cb27319ed49c13f7a5cb668
Daniel Boline <ddboline@gmail.com>
2020-02-29 14:49:02 -0500

    use login_html(), remove unused templates, version bump

--- garmin_http/src/errors.rs
+++ garmin_http/src/errors.rs
@@ -4,6 +4,7 @@ use anyhow::Error as AnyhowError;
 use std::fmt::Debug;
 use thiserror::Error;
 use tokio::task::JoinError;
+use rust_auth_server::static_files::login_html;
 
 use crate::logged_user::TRIGGER_DB_UPDATE;
 
@@ -32,13 +33,7 @@ impl ResponseError for ServiceError {
             Self::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
             Self::Unauthorized => {
                 TRIGGER_DB_UPDATE.set();
-                HttpResponse::Ok()
-                    .content_type("text/html; charset=utf-8")
-                    .body(
-                        include_str!("../../templates/login.html")
-                            .replace("main.css", "/auth/main.css")
-                            .replace("main.js", "/auth/main.js"),
-                    )
+                login_html()
             }
             _ => {
                 HttpResponse::InternalServerError().json("Internal Server Error, Please try later")

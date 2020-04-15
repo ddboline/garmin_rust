1be4683d5dfe6b7309ef6e4a4555a49ca5709f0f
Daniel Boline <ddboline@gmail.com>
2020-04-15 13:09:56 -0400

    refactor StackString

--- garmin_lib/src/utils/stack_string.rs
+++ garmin_lib/src/utils/stack_string.rs
@@ -3,8 +3,12 @@ use bytes::BytesMut;
 use derive_more::{Display, From, Into};
 use inlinable_string::InlinableString;
 use serde::{Deserialize, Serialize};
-use std::{borrow::Borrow, str::FromStr};
+use std::{borrow::Borrow, str::FromStr, string::{FromUtf16Error, FromUtf8Error}};
 use tokio_postgres::types::{FromSql, IsNull, ToSql, Type};
+use std::ops::{Deref, DerefMut};
+pub use inlinable_string::StringExt;
+use std::borrow::Cow;
+
 
 #[derive(
     Serialize,
@@ -75,19 +79,6 @@ impl Borrow<str> for StackString {
     }
 }
 
-impl AsRef<str> for StackString {
-    fn as_ref(&self) -> &str {
-        self.as_str()
-    }
-}
-
-impl FromStr for StackString {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(s.into())
-    }
-}
-
 impl<'a> FromSql<'a> for StackString {
     fn from_sql(
         ty: &Type,
@@ -127,5 +118,168 @@ impl ToSql for StackString {
         out: &mut BytesMut,
     ) -> Result<IsNull, Box<dyn std::error::Error + Sync + Send>> {
         self.to_string().to_sql_checked(ty, out)
+    }
+}
+
+impl Deref for StackString {
+    type Target = str;
+
+    #[inline]
+    fn deref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+impl DerefMut for StackString {
+    fn deref_mut(&mut self) -> &mut str {
+        self.0.as_mut()
+    }
+}
+
+impl AsRef<str> for StackString {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl FromStr for StackString {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(s.into())
+    }
+}
+
+impl<'a> PartialEq<Cow<'a, str>> for StackString {
+    #[inline]
+    fn eq(&self, other: &Cow<'a, str>) -> bool {
+        PartialEq::eq(&self[..], &other[..])
+    }
+}
+
+impl<'a> PartialEq<String> for StackString {
+    #[inline]
+    fn eq(&self, other: &String) -> bool {
+        PartialEq::eq(&self[..], &other[..])
+    }
+}
+
+impl<'a> PartialEq<str> for StackString {
+    #[inline]
+    fn eq(&self, other: &str) -> bool {
+        PartialEq::eq(&self[..], &other[..])
+    }
+}
+
+impl<'a> PartialEq<&'a str> for StackString {
+    #[inline]
+    fn eq(&self, other: &&'a str) -> bool {
+        PartialEq::eq(&self[..], &other[..])
+    }
+}
+
+impl<'a> StringExt<'a> for StackString {
+    #[inline]
+    fn new() -> Self {
+        StackString(InlinableString::new())
+    }
+
+    #[inline]
+    fn with_capacity(capacity: usize) -> Self {
+        StackString(InlinableString::with_capacity(capacity))
+    }
+
+    #[inline]
+    fn from_utf8(vec: Vec<u8>) -> Result<Self, FromUtf8Error> {
+        InlinableString::from_utf8(vec).map(StackString)
+    }
+
+    #[inline]
+    fn from_utf16(v: &[u16]) -> Result<Self, FromUtf16Error> {
+        InlinableString::from_utf16(v).map(StackString)
+    }
+
+    #[inline]
+    fn from_utf16_lossy(v: &[u16]) -> Self {
+        StackString(InlinableString::from_utf16_lossy(v))
+    }
+
+    #[inline]
+    unsafe fn from_raw_parts(buf: *mut u8, length: usize, capacity: usize) -> Self {
+        StackString(InlinableString::from_raw_parts(buf, length, capacity))
+    }
+
+    #[inline]
+    unsafe fn from_utf8_unchecked(bytes: Vec<u8>) -> Self {
+        StackString(InlinableString::from_utf8_unchecked(bytes))
+    }
+
+    #[inline]
+    fn into_bytes(self) -> Vec<u8> {
+        InlinableString::into_bytes(self.0)
+    }
+
+    #[inline]
+    fn push_str(&mut self, string: &str) {
+        InlinableString::push_str(&mut self.0, string)
+    }
+
+    #[inline]
+    fn capacity(&self) -> usize {
+        InlinableString::capacity(&self.0)
+    }
+
+    #[inline]
+    fn reserve(&mut self, additional: usize) {
+        InlinableString::reserve(&mut self.0, additional)
+    }
+
+    #[inline]
+    fn reserve_exact(&mut self, additional: usize) {
+        InlinableString::reserve_exact(&mut self.0, additional)
+    }
+
+    #[inline]
+    fn shrink_to_fit(&mut self) {
+        InlinableString::shrink_to_fit(&mut self.0)
+    }
+
+    #[inline]
+    fn push(&mut self, ch: char) {
+        InlinableString::push(&mut self.0, ch)
+    }
+
+    #[inline]
+    fn as_bytes(&self) -> &[u8] {
+        InlinableString::as_bytes(&self.0)
+    }
+
+    #[inline]
+    fn truncate(&mut self, new_len: usize) {
+        InlinableString::truncate(&mut self.0, new_len)
+    }
+
+    #[inline]
+    fn pop(&mut self) -> Option<char> {
+        InlinableString::pop(&mut self.0)
+    }
+
+    #[inline]
+    fn remove(&mut self, idx: usize) -> char {
+        InlinableString::remove(&mut self.0, idx)
+    }
+
+    #[inline]
+    fn insert(&mut self, idx: usize, ch: char) {
+        InlinableString::insert(&mut self.0, idx, ch)
+    }
+
+    #[inline]
+    unsafe fn as_mut_slice(&mut self) -> &mut [u8] {
+        InlinableString::as_mut_slice(&mut self.0)
+    }
+
+    #[inline]
+    fn len(&self) -> usize {
+        InlinableString::len(&self.0)
     }
 }

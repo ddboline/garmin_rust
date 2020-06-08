e719ea0bccc552ea22cc3441c2c0385acc9e0ac0
Daniel Boline <ddboline@gmail.com>
2020-06-08 17:18:17 -0400

    add not null's

--- scripts/garmin_summary.sql
+++ scripts/garmin_summary.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS garmin_summary (
     filename text NOT NULL PRIMARY KEY,
     begin_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
     sport varchar(12) NOT NULL,
-    total_calories integer,
-    total_distance double precision,
-    total_duration double precision,
-    total_hr_dur double precision,
-    total_hr_dis double precision,
-    md5sum varchar(32)
+    total_calories integer NOT NULL,
+    total_distance double precision NOT NULL,
+    total_duration double precision NOT NULL,
+    total_hr_dur double precision NOT NULL,
+    total_hr_dis double precision NOT NULL,
+    md5sum varchar(32) NOT NULL
 );

a9c282347d37598f5d684a88936a59959576f3ac
Daniel Boline <ddboline@gmail.com>
2020-07-29 14:15:25 -0400

    add interactivity to LINEPLOTTEMPLATE

--- templates/LINEPLOTTEMPLATE.js
+++ templates/LINEPLOTTEMPLATE.js
@@ -24,7 +24,8 @@ var svg = d3.select("body")
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
         .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+              "translate(" + margin.left + "," + margin.top + ")")
+    .on("mousemove touchmove", handleMouseOverData);
 
 svg.append("text")      // text label for chart Title
         .attr("x", width / 2 )
@@ -67,3 +68,40 @@ y.domain([ymin, ymax]);
 svg.append("path").attr("class", "line").attr("d", valueline(data));
 svg.append("g").attr("class", "xaxis").attr("transform", "translate(0," + height + ")").call(xAxis);
 svg.append("g").attr("class", "yaxis").call(yAxis);
+
+let rule = svg.append("g")
+    .append("line")
+      .attr("y1", y(ymin))
+      .attr("y2", y(ymax))
+      .attr("stroke", "black");
+
+function handleMouseOverData() {
+    let d = d3.mouse(this)
+    let date = x.invert(d[0]);
+    let heartrate = y.invert(d[1]);
+
+    rule.attr("transform", `translate(${d[0]}, 0)`);
+
+    svg.property("value", date).dispatch("input");
+    d3.event.preventDefault();
+
+    let data_date = d3.select('#data_date');
+    if (data_date) {
+        data_date.remove();
+    }
+    let data_heartrate = d3.select('#data_heartrate');
+    if (data_heartrate) {
+        data_heartrate.remove();
+    }
+
+    svg.append('text')
+        .attr("id", 'data_date')
+        .attr("x", function() {return x(xmin) + 30;})
+        .attr("y", function() {return y(ymax) + 15;})
+        .text(function() {return date;});
+    svg.append('text')
+        .attr("id", 'data_heartrate')
+        .attr("x", function() {return x(xmin) + 30;})
+        .attr("y", function() {return y(ymax) + 30;})
+        .text(function() {return heartrate;});
+}

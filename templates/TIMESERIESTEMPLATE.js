8e17c5c0404f712dd5c0cd61879a84dc6668dacf
Daniel Boline <ddboline@gmail.com>
2020-07-29 14:07:13 -0400

    add interactivity to timeseries

--- templates/TIMESERIESTEMPLATE.js
+++ templates/TIMESERIESTEMPLATE.js
@@ -14,11 +14,6 @@ var y = d3.scaleLinear().range([height, 0]);
 var xAxis = d3.axisBottom(x).ticks(5);
 
 var yAxis = d3.axisLeft(y).ticks(5);
-
-// Define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d[0]); })
-    .y(function(d) { return y(d[1]); });
     
 // Adds the svg canvas
 var svg = d3.select("body")
@@ -27,7 +22,8 @@ var svg = d3.select("body")
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
         .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+              "translate(" + margin.left + "," + margin.top + ")")
+        .on("mousemove touchmove", handleMouseOverData);
 
 svg.append("text")      // text label for chart Title
         .attr("x", width / 2 )
@@ -57,6 +53,8 @@ data.forEach(function(d) {
     d[0] = parseDateTime(d[0]);
 });
 
+var xmax = d3.max(data, function(d) {return d[0]});
+var xmin = d3.min(data, function(d) {return d[0]});
 var ymax = d3.max(data, function(d) {return d[1]});
 var ymin = d3.min(data, function(d) {return d[1]});
 
@@ -65,6 +63,11 @@ ymin = ymin - 0.1 * Math.abs(ymin);
 
 x.domain(d3.extent(data, function(d) {return d[0]; }));
 y.domain([ymin, ymax]);
+
+// Define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d[0]); })
+    .y(function(d) { return y(d[1]); });
 
 svg.append("path").attr("class", "line").attr("d", valueline(data));
 
@@ -101,24 +104,39 @@ function wrap(text, width) {
     });
 }
 
-function handleMouseOverData(d, i) {
-    svg.append('line')
-        .attr("id", "data_line")
-        .attr("x1", x(d[0]))
-        .attr("y1", y(ymin))
-        .attr("x2", x(d[0]))
-        .attr("y2", y(ymax))
-        .style("stroke-width", 2)
-        .style("stroke", "black")
-        .style("fill", "none");
-    svg.append('text')
-        .attr("id", 'data')
-        .attr("x", function() {return x(xmin) + 30;})
-        .attr("y", function() {return y(ymax) + 15;})
-        .text(function() {return d[0];});
+let rule = svg.append("g")
+    .append("line")
+      .attr("y1", y(ymin))
+      .attr("y2", y(ymax))
+      .attr("stroke", "black");
+
+function handleMouseOverData() {
+    let d = d3.mouse(this)
+    let date = x.invert(d[0]);
+    let heartrate = y.invert(d[1]);
+
+    rule.attr("transform", `translate(${d[0]}, 0)`);
+
+    svg.property("value", date).dispatch("input");
+    d3.event.preventDefault();
+
+    let data_date = d3.select('#data_date');
+    if (data_date) {
+        data_date.remove();
+    }
+    let data_heartrate = d3.select('#data_heartrate');
+    if (data_heartrate) {
+        data_heartrate.remove();
+    }
+
     svg.append('text')
         .attr("id", 'data_date')
         .attr("x", function() {return x(xmin) + 30;})
+        .attr("y", function() {return y(ymax) + 15;})
+        .text(function() {return date;});
+    svg.append('text')
+        .attr("id", 'data_heartrate')
+        .attr("x", function() {return x(xmin) + 30;})
         .attr("y", function() {return y(ymax) + 30;})
-        .text(function() {return d[1];});
+        .text(function() {return heartrate;});
 }

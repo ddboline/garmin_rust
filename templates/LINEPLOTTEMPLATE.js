6709769da1ad1b3ada7713fbf3a47c01600dc771
Daniel Boline <ddboline@gmail.com>
2020-07-29 14:52:28 -0400

    more hackery

--- templates/LINEPLOTTEMPLATE.js
+++ templates/LINEPLOTTEMPLATE.js
@@ -1,16 +1,16 @@
 // Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 60},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+let margin_NAME = {top: 30, right: 20, bottom: 30, left: 60};
+let width_NAME = 600 - margin_NAME.left - margin_NAME.right;
+let height_NAME = 270 - margin_NAME.top - margin_NAME.bottom;
 
 // Set the ranges
-let x_NAME = d3.scaleLinear().range([0, width]);
-let y_NAME = d3.scaleLinear().range([height, 0]);
+let x_NAME = d3.scaleLinear().range([0, width_NAME]);
+let y_NAME = d3.scaleLinear().range([height_NAME, 0]);
 
 // Define the axes
-var xAxis = d3.axisBottom(x_NAME).ticks(5);
+let xAxis_NAME = d3.axisBottom(x_NAME).ticks(5);
 
-var yAxis = d3.axisLeft(y_NAME).ticks(5);
+let yAxis_NAME = d3.axisLeft(y_NAME).ticks(5);
 
 // Define the line
 var valueline = d3.line()
@@ -20,42 +20,42 @@ var valueline = d3.line()
 // Adds the svg canvas
 var svg = d3.select("body")
     .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width_NAME + margin_NAME.left + margin_NAME.right)
+        .attr("height", height_NAME + margin_NAME.top + margin_NAME.bottom)
     .append("g")
         .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")")
-    .on("mousemove touchmove", handleMouseOverData);
+              "translate(" + margin_NAME.left + "," + margin_NAME.top + ")")
+    .on("mousemove touchmove", handleMouseOverData_NAME);
 
 svg.append("text")      // text label for chart Title
-        .attr("x", width / 2 )
-        .attr("y", 0 - (margin.top/2))
+        .attr("x", width_NAME / 2 )
+        .attr("y", 0 - (margin_NAME.top/2))
         .style("text-anchor", "middle")
 		.style("font-size", "16px")
         .style("text-decoration", "underline")
         .text("EXAMPLETITLE");
 
 svg.append("text")      // text label for the x-axis
-        .attr("x", width / 2 )
-        .attr("y",  height + margin.bottom)
+        .attr("x", width_NAME / 2 )
+        .attr("y",  height_NAME + margin_NAME.bottom)
         .style("text-anchor", "middle")
         .text("XAXIS");
 
 svg.append("text")      // text label for the y-axis
-        .attr("y",30 - margin.left)
-        .attr("x",50 - (height / 2))
+        .attr("y",30 - margin_NAME.left)
+        .attr("x",50 - (height_NAME / 2))
         .attr("transform", "rotate(-90)")
         .style("text-anchor", "end")
         .style("font-size", "16px")
         .text("YAXIS");
 
 // Get the data
-var data = DATA;
+let data_NAME = DATA;
 
-var xmax_NAME = d3.max(data, function(d) {return d[0]});
-var xmin_NAME = d3.min(data, function(d) {return d[0]});
-var ymax_NAME = d3.max(data, function(d) {return d[1]});
-var ymin_NAME = d3.min(data, function(d) {return d[1]});
+let xmax_NAME = d3.max(data_NAME, function(d) {return d[0]});
+let xmin_NAME = d3.min(data_NAME, function(d) {return d[0]});
+let ymax_NAME = d3.max(data_NAME, function(d) {return d[1]});
+let ymin_NAME = d3.min(data_NAME, function(d) {return d[1]});
 
 xmax_NAME = xmax_NAME + 0.1 * Math.abs(xmax_NAME);
 xmin_NAME = xmin_NAME - 0.1 * Math.abs(xmin_NAME);
@@ -65,9 +65,9 @@ ymin_NAME = ymin_NAME - 0.1 * Math.abs(ymin_NAME);
 x_NAME.domain([xmin_NAME, xmax_NAME]);
 y_NAME.domain([ymin_NAME, ymax_NAME]);
 
-svg.append("path").attr("class", "line").attr("d", valueline(data));
-svg.append("g").attr("class", "xaxis").attr("transform", "translate(0," + height + ")").call(xAxis);
-svg.append("g").attr("class", "yaxis").call(yAxis);
+svg.append("path").attr("class", "line").attr("d", valueline(data_NAME));
+svg.append("g").attr("class", "xaxis").attr("transform", "translate(0," + height_NAME + ")").call(xAxis_NAME);
+svg.append("g").attr("class", "yaxis").call(yAxis_NAME);
 
 let rule_NAME = svg.append("g")
     .append("line")
@@ -75,7 +75,7 @@ let rule_NAME = svg.append("g")
       .attr("y2", y_NAME(ymax_NAME))
       .attr("stroke", "black");
 
-function handleMouseOverData() {
+function handleMouseOverData_NAME() {
     let d = d3.mouse(this)
     let date = x_NAME.invert(d[0]);
     let heartrate = y_NAME.invert(d[1]);

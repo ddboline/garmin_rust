75c67fe7e840097535d9147aa5ce9550c182735b
Daniel Boline <ddboline@gmail.com>
2020-08-07 11:53:06 -0400

    refactor out Client::raw_client_for calls, change MODERN_URL from str to Url

--- garmin_connect_lib/src/garmin_connect_client.rs
+++ garmin_connect_lib/src/garmin_connect_client.rs
@@ -1,6 +1,8 @@
 use anyhow::{format_err, Error};
+use bytes::Bytes;
 use chrono::{DateTime, NaiveDate, Utc};
-use fantoccini::{Client, Locator};
+use fantoccini::{Client, Locator, Method};
+use lazy_static::lazy_static;
 use log::debug;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
@@ -18,7 +20,11 @@ use garmin_lib::common::{
 
 use super::garmin_connect_hr_data::GarminConnectHrData;
 
-const MODERN_URL: &str = "https://connect.garmin.com/modern";
+lazy_static! {
+    static ref MODERN_URL: Url = "https://connect.garmin.com/modern"
+        .parse()
+        .expect("Bad URL");
+}
 
 pub struct GarminConnectClient {
     config: GarminConfig,
@@ -26,6 +32,7 @@ pub struct GarminConnectClient {
     webdriver: Option<Child>,
     pub last_used: DateTime<Utc>,
     display_name: Option<StackString>,
+    trigger_auth: bool,
 }
 
 impl Default for GarminConnectClient {
@@ -43,12 +50,19 @@ impl GarminConnectClient {
             webdriver: None,
             last_used: Utc::now(),
             display_name: None,
+            trigger_auth: true,
         }
     }
 
     pub async fn init(&mut self, config: GarminConfig) -> Result<(), Error> {
         self.config = config;
-        if self.webdriver.is_none() && self.config.webdriver_path.exists() {
+        if !self.config.webdriver_path.exists() {
+            return Err(format_err!(
+                "WEBDRIVER NOT FOUND {:?}",
+                self.config.webdriver_path
+            ));
+        }
+        if self.trigger_auth {
             let webdriver = Command::new(&self.config.webdriver_path)
                 .args(&[&format!("--port={}", self.config.webdriver_port)])
                 .kill_on_drop(true)
@@ -57,8 +71,7 @@ impl GarminConnectClient {
                 .spawn()?;
             self.webdriver.replace(webdriver);
             delay_for(std::time::Duration::from_secs(5)).await;
-        }
-        if self.client.is_none() {
+
             let mut caps = serde_json::map::Map::new();
             let opts = serde_json::json!({
                 "args": ["--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"],
@@ -75,11 +88,19 @@ impl GarminConnectClient {
                 .await?,
             );
             self.last_used = Utc::now();
+            self.trigger_auth = false;
         }
         if self.display_name.is_none() {
             self.authorize().await?;
         }
         Ok(())
+    }
+
+    async fn raw_get(client: &mut Client, url: &Url) -> Result<Bytes, Error> {
+        let raw = client.raw_client_for(Method::GET, url.as_str()).await?;
+        hyper::body::to_bytes(raw.into_body())
+            .await
+            .map_err(Into::into)
     }
 
     pub async fn authorize(&mut self) -> Result<(), Error> {
@@ -96,30 +117,31 @@ impl GarminConnectClient {
             .submit()
             .await?;
 
-        client.goto(MODERN_URL).await?;
-        let raw = client
-            .raw_client_for(fantoccini::Method::GET, MODERN_URL)
-            .await?;
-        self.last_used = Utc::now();
-        let js = hyper::body::to_bytes(raw.into_body()).await?;
+        client.goto(MODERN_URL.as_str()).await?;
+        let js = Self::raw_get(client, &MODERN_URL).await?;
         let text = std::str::from_utf8(&js)?;
+        self.last_used = Utc::now();
 
         self.display_name
             .replace(GarminConnectClient::extract_display_name(text)?);
-
+        if self.display_name.is_none() {
+            self.trigger_auth = true;
+        }
         Ok(())
     }
 
     pub async fn close(&mut self) -> Result<(), Error> {
+        if let Some(mut client) = self.client.take() {
+            client.close().await?;
+        }
         if let Some(mut webdriver) = self.webdriver.take() {
             if let Err(e) = webdriver.kill() {
                 debug!("Failed to kill {}", e);
             }
         }
-        if let Some(mut client) = self.client.take() {
-            client.close().await?;
-        }
         self.last_used = Utc::now();
+        self.display_name.take();
+        self.trigger_auth = true;
         Ok(())
     }
 
@@ -152,17 +174,18 @@ impl GarminConnectClient {
             .display_name
             .as_ref()
             .ok_or_else(|| format_err!("No display name"))?;
-        let url_prefix = format!(
-            "{}/proxy/usersummary-service/usersummary/daily/{}",
-            MODERN_URL, display_name,
-        );
-        let url = Url::parse_with_params(&url_prefix, &[("calendarDate", &date.to_string())])?;
-        let raw = client
-            .raw_client_for(fantoccini::Method::GET, url.as_str())
-            .await?;
+        let mut url = MODERN_URL
+            .join("/proxy/usersummary-service/usersummary/daily/")?
+            .join(display_name)?;
+        url.query_pairs_mut()
+            .append_pair("calendarDate", &date.to_string());
+        let js = Self::raw_get(client, &url).await?;
+        let user_summary: GarminConnectUserDailySummary = serde_json::from_slice(&js)?;
         self.last_used = Utc::now();
-        let js = hyper::body::to_bytes(raw.into_body()).await?;
-        serde_json::from_slice(&js).map_err(Into::into)
+        if user_summary.total_steps.is_none() {
+            self.trigger_auth = true;
+        }
+        Ok(user_summary)
     }
 
     pub async fn get_heartrate(&mut self, date: NaiveDate) -> Result<GarminConnectHrData, Error> {
@@ -174,16 +197,12 @@ impl GarminConnectClient {
             .client
             .as_mut()
             .ok_or_else(|| format_err!("No client"))?;
-        let url = format!(
-            "{}/proxy/wellness-service/wellness/dailyHeartRate/{}",
-            MODERN_URL, display_name
-        );
-        let url = Url::parse_with_params(&url, &[("date", &date.to_string())])?;
-        let raw = client
-            .raw_client_for(fantoccini::Method::GET, url.as_str())
-            .await?;
+        let mut url = MODERN_URL
+            .join("/proxy/wellness-service/wellness/dailyHeartRate/")?
+            .join(display_name)?;
+        url.query_pairs_mut().append_pair("date", &date.to_string());
+        let js = Self::raw_get(client, &url).await?;
         self.last_used = Utc::now();
-        let js = hyper::body::to_bytes(raw.into_body()).await?;
         serde_json::from_slice(&js).map_err(Into::into)
     }
 
@@ -195,13 +214,9 @@ impl GarminConnectClient {
             .client
             .as_mut()
             .ok_or_else(|| format_err!("No client"))?;
-        let url = format!(
-            "{}/proxy/activitylist-service/activities/search/activities",
-            MODERN_URL
-        );
-        let raw = client.raw_client_for(fantoccini::Method::GET, &url).await?;
+        let url = MODERN_URL.join("/proxy/activitylist-service/activities/search/activities")?;
+        let js = Self::raw_get(client, &url).await?;
         self.last_used = Utc::now();
-        let js = hyper::body::to_bytes(raw.into_body()).await?;
         serde_json::from_slice(&js).map_err(Into::into)
     }
 
@@ -221,15 +236,11 @@ impl GarminConnectClient {
                 .join("Downloads")
                 .join(activity.activity_id.to_string())
                 .with_extension("zip");
-            let url = format!(
-                "{}/proxy/download-service/files/activity/{}",
-                MODERN_URL, activity.activity_id
-            );
-            let raw = client
-                .raw_client_for(fantoccini::Method::GET, url.as_str())
-                .await?;
+            let url = MODERN_URL
+                .join("/proxy/download-service/files/activity/")?
+                .join(&activity.activity_id.to_string())?;
+            let data = Self::raw_get(client, &url).await?;
             self.last_used = Utc::now();
-            let data = hyper::body::to_bytes(raw.into_body()).await?;
             fs::write(&fname, &data).await?;
             filenames.push(fname);
         }

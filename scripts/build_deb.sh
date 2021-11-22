d7ad839314131eaf82119b663b4fb17c0dab95e3
Daniel Boline <ddboline@gmail.com>
2021-11-21 20:03:57 -0500

    remove docker stuff

--- scripts/build_deb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VERSION="$1"
-RELEASE="$2"
-
-printf "Process and display info about gps activity files\n" > description-pak
-checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} -y

143435ee181797b8d59be2846bb9d0d14ba20f59
Daniel Boline <ddboline@gmail.com>
2021-12-06 22:58:20 -0500

    version bump

--- garmin_http/src/logged_user.rs
+++ garmin_http/src/logged_user.rs
@@ -128,7 +128,9 @@ impl TryFrom<Token> for LoggedUser {
 impl FromStr for LoggedUser {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let token: Token = s.to_string().into();
+        let mut buf = StackString::new();
+        buf.push_str(s);
+        let token: Token = buf.into();
         token.try_into()
     }
 }
@@ -168,7 +170,7 @@ pub async fn fill_from_db(pool: &PgPool) -> Result<(), Error> {
         AUTHORIZED_USERS.get_users()
     };
     if let Ok("true") = var("TESTENV").as_ref().map(String::as_str) {
-        AUTHORIZED_USERS.merge_users(&["user@test".into()])?;
+        AUTHORIZED_USERS.merge_users(["user@test"])?;
     }
     AUTHORIZED_USERS.merge_users(&users)?;
     debug!("{:?}", *AUTHORIZED_USERS);

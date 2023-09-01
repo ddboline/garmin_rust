69dda6f64c6bb713738ca0eab561565b70badc6c
Daniel Boline <ddboline@gmail.com>
2023-09-01 18:52:31 -0400

    heartrates

--- src/import_garmin_connect_data.rs
+++ src/import_garmin_connect_data.rs
@@ -8,9 +8,16 @@
 use anyhow::Error;
 use clap::Parser;
 use std::path::PathBuf;
+use tempdir::TempDir;
 
-use fitbit_lib::fitbit_heartrate::{import_garmin_json_file, import_garmin_heartrate_file};
-use garmin_lib::common::garmin_connect_activity::import_garmin_connect_activity_json_file;
+use fitbit_lib::fitbit_heartrate::{import_garmin_heartrate_file, import_garmin_json_file};
+use garmin_lib::{
+    common::{
+        garmin_config::GarminConfig,
+        garmin_connect_activity::import_garmin_connect_activity_json_file,
+    },
+    utils::garmin_util::extract_zip_from_garmin_connect_multiple,
+};
 
 #[derive(Parser, Debug, Clone)]
 enum JsonImportOpts {
@@ -24,11 +31,17 @@ enum JsonImportOpts {
         #[clap(short = 'f', long = "files")]
         files: Vec<PathBuf>,
     },
+    #[clap(alias = "hrs")]
+    Heartrates {
+        #[clap(short = 'f', long = "file")]
+        file: PathBuf,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
+    let config = GarminConfig::get_config(None)?;
     let opts = JsonImportOpts::parse();
 
     match opts {
@@ -37,8 +50,20 @@ async fn main() -> Result<(), Error> {
         }
         JsonImportOpts::Heartrate { files } => {
             for file in files {
-                // import_garmin_json_file(&file)?;
-                import_garmin_heartrate_file(&file)?;
+                import_garmin_json_file(&config, &file)?;
+                if import_garmin_json_file(&config, &file).is_err() {
+                    import_garmin_heartrate_file(&config, &file)?;
+                }
+            }
+        }
+        JsonImportOpts::Heartrates { file } => {
+            let tempdir = TempDir::new("garmin_zip")?;
+            let ziptmpdir = tempdir.path();
+            let files = extract_zip_from_garmin_connect_multiple(&file, ziptmpdir)?;
+            for file in files {
+                if import_garmin_heartrate_file(&config, &file).is_ok() {
+                    println!("processed {}", file.to_string_lossy());
+                }
             }
         }
     }
